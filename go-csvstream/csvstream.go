f659d71e6ad6b35659ecd90752a2a0962b02e562
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-03-01 16:24:15 -0800

    go package for async csv streaming

---
+++ go-csvstream/csvstream.go
@@ -0,0 +1,45 @@
+// csvstream.go [created: Sat,  1 Mar 2014]
+
+/*
+Package csvstream provides simple asynchronous streaming for CSV data.
+It requires continuation logic to be implemented by the application as
+errors depend of the configuration of csv.Reader structs.
+*/
+package csvstream
+
+import (
+	"encoding/csv"
+	"io"
+)
+
+type Row struct {
+	Cols []string
+	Err  error
+}
+
+type Stream <-chan *Row
+
+// read from r asynchronously. the returned channel is closed on EOF.
+// if there is an error reading from r, the error is passed over the
+// channel and the channel is closed. It is up to the application to
+// decide if a new Stream should be created to continue reading from
+// r.
+func New(r *csv.Reader, bufsize uint) Stream {
+	ch := make(chan *Row, bufsize)
+	go func() {
+		defer close(ch)
+		for {
+			cols, err := r.Read()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				ch <- &Row{nil, err}
+				return
+			}
+			ch <- &Row{cols, nil}
+		}
+		panic("unreachable")
+	}()
+	return ch
+}
